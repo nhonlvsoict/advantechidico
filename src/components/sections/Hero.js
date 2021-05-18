ac7ca0ca01d02329463fa036cd96399036163c9b
nhonlv <nhonlv.soict@gmail.com>
2021-05-18 17:20:55 +0700

    Initial Commit

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -84,14 +84,14 @@ const Hero = ({
           </div>
           <div className="hero-figure reveal-from-bottom illustration-element-01" data-reveal-value="20px" data-reveal-delay="800">
             <a
-              data-video="https://youtu.be/alnmbGAOszY"
+              data-video="https://www.youtube.com/embed/alnmbGAOszY"
               href="#0"
               aria-controls="video-modal"
               onClick={openModal}
             >
               <Image
                 className="has-shadow"
-                src={require('./../../assets/images/video-placeholder.jpg')}
+                src={require('./../../assets/images/idicoVideo.PNG')}
                 alt="Hero"
                 width={896}
                 height={504} />
@@ -101,7 +101,7 @@ const Hero = ({
             id="video-modal"
             show={videoModalActive}
             handleClose={closeModal}
-            video="https://youtu.be/alnmbGAOszY"
+            video="https://www.youtube.com/embed/alnmbGAOszY"
             videoTag="iframe" />
         </div>
       </div>
